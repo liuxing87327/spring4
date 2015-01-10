6e9b29b72222e894c78ae1bda602d2359826644c
刘兴 <liuxing87327@163.com>
2015-01-11 01:10:04 +0800

    优化依赖配置
    优化配置文件
    加入log4j

--- spring-websocket/src/main/java/com/dooioo/index/controller/IndexController.java
+++ spring-websocket/src/main/java/com/dooioo/index/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.dooioo.index.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,12 +20,15 @@ import java.util.Map;
 @RequestMapping(value="/index")
 public class IndexController {
 
+    private static Log logger = LogFactory.getLog(IndexController.class);
+
     /**
      * 进入默认首页
      * @return
      */
     @RequestMapping(method = RequestMethod.GET)
     public String helloWorld() {
+        logger.info("进入默认首页");
         return "index";
     }
 
