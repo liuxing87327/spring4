b0454cd9ac01f97ea3794c0f035ebac3a52acc23
刘兴 <liuxing87327@163.com>
2015-07-03 02:22:37 +0800

    使用池消费

--- spring-jms/src/main/java/com/liuxing/service/impl/ProducerServiceImpl.java
+++ spring-jms/src/main/java/com/liuxing/service/impl/ProducerServiceImpl.java
@@ -3,14 +3,10 @@ package com.liuxing.service.impl;
 
 import com.liuxing.service.ProducerService;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import javax.jms.Destination;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Session;
 
 /**
  * @title：ProducerServiceImpl
@@ -25,10 +21,8 @@ public class ProducerServiceImpl implements ProducerService {
     public void sendMessage(Destination destination, final String message) {
         System.out.println("---------------生产者发送消息-----------------");
         System.out.println("---------------生产者发了一个消息：" + message);
-        jmsTemplate.send(destination, new MessageCreator() {
-            public Message createMessage(Session session) throws JMSException {
-                return session.createTextMessage(message);
-            }
+        jmsTemplate.send(destination, session -> {
+            return session.createTextMessage(message);
         });
     }
 
--- spring-jms/src/test/java/com/liuxing/service/ProducerServiceTest.java
+++ spring-jms/src/test/java/com/liuxing/service/ProducerServiceTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import javax.jms.Destination;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @title：ProducerServiceTest
@@ -22,9 +24,14 @@ public class ProducerServiceTest extends BaseTest {
 
     @Test
     public void testSend() {
-        for (int i = 0; i < 1000; i++) {
-            producerService.sendMessage(destination, "你好，生产者！这是消息：" + (i + 1));
+        Set<Integer> list = new HashSet<>();
+        for (int i = 1; i < 1000000; i++) {
+            list.add(i);
         }
+
+        list.parallelStream().forEach(i -> {
+            producerService.sendMessage(destination, "你好，生产者！这是消息：" + i);
+        });
     }
 
 }
